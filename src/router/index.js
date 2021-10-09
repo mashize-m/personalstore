b2e6a7c695fee2ce9f3636275f093d11e50c72bd
mashize <137854126@qq.com>
2021-10-09 10:39:00 +0800

    项目模块框架搭建划分

---
+++ src/router/index.js
@@ -0,0 +1,42 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+
+// 懒加载
+const Home = () => import("../views/home/Home.vue");
+const Category = () => import("../views/category/Category.vue");
+const Profile = () => import("../views/profile/Profile.vue");
+const Shopcart = () => import("../views/shopcart/Shopcart.vue");
+
+// 1. 安装插件
+Vue.use(VueRouter);
+
+// 2. 创建实例
+const routes = [
+  {
+    path: "",
+    redirect: "/home"
+  },
+  {
+    path: "/home",
+    component: Home
+  },
+  {
+    path: "/category",
+    component: Category
+  },
+  {
+    path: "/profile",
+    component: Profile
+  },
+  {
+    path: "/shopcart",
+    component: Shopcart
+  }
+];
+const router = new VueRouter({
+  routes,
+  mode: "history"
+});
+
+// 3. 导出router
+export default router;
---
+++ src/views/category/Category.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>分类</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    };
+  },
+
+  components: {},
+
+  computed: {},
+
+  methods: {}
+}
+</script>
+<style lang='css' scoped>
+</style>
